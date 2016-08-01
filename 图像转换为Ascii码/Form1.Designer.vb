a6e745251c29c8977f471204e5118996fe6c1083
Leon <xiaoyankeji@hotmail.com>
2016-08-01 18:28:27 +0800

    第一次提交

---
+++ 图像转换为Ascii码/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CustomImageDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
+        Me.复制到剪切板ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GIFTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(544, 328)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.WordWrap = False
+        '
+        'CustomImageDialog
+        '
+        Me.CustomImageDialog.Filter = "Image Files|*.jpg;*.jpeg;*.gif;*.png;*.bmp;*.wmf|All Files|*.*"
+        Me.CustomImageDialog.Title = "Please select the path of image file."
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.ToolStripTextBox1, Me.复制到剪切板ToolStripMenuItem, Me.ToolStripMenuItem2})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(544, 27)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(68, 23)
+        Me.ToolStripMenuItem1.Text = "选择图像"
+        '
+        'ToolStripTextBox1
+        '
+        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.ToolStripTextBox1.Text = "5"
+        '
+        '复制到剪切板ToolStripMenuItem
+        '
+        Me.复制到剪切板ToolStripMenuItem.Name = "复制到剪切板ToolStripMenuItem"
+        Me.复制到剪切板ToolStripMenuItem.Size = New System.Drawing.Size(92, 23)
+        Me.复制到剪切板ToolStripMenuItem.Text = "复制到剪切板"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(80, 23)
+        Me.ToolStripMenuItem2.Text = "保存为图片"
+        '
+        'GIFTimer
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 355)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.DoubleBuffered = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Leon.ID@QQ.COM"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents CustomImageDialog As OpenFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripTextBox1 As ToolStripTextBox
+    Friend WithEvents 复制到剪切板ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GIFTimer As Timer
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+End Class
---
+++ 图像转换为Ascii码/Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim CellSize As Size = New Size(5, 10)
+    Dim AsciiCount As Int16 = 32
+    Dim Ascii() As Char = {" ", "`", ".", "^", ",", ":", "~", """", "<", "!", "c", "t", "+", "{", "i", "7", "?", "u", "3", "0", "p", "w", "4", "A", "8", "D", "X", "%", "#", "H", "W", "M"}
+    Dim AscGray() As Byte = {0, 14, 19, 25, 36, 42, 48, 53, 59, 65, 70, 76, 82, 87, 93, 99, 104, 110, 116, 121, 127, 133, 138, 144, 150, 155, 167, 172, 178, 187, 192, 198}
+    Dim TestBitmap As Bitmap
+    Dim mFrameCount As Integer
+    Dim mTimeDimension As FrameDimension
+    Dim FrameBitmap As Bitmap
+    Dim FrameGraphics As Graphics
+    Dim mFrameIndex As Integer
+
+    '@Leon：算法原理：
+    '   1>.对图像进行去色处理转换为灰阶图
+    '   2>.对灰阶图分块取灰度
+    '   3>.匹配灰度最接近的Ascii码
+
+    '# 图像转换为Ascii码 #
+    'Function BitmapToAscii(ByVal GrayScaleBitmap As Bitmap, ByVal CellSize As Size) As String
+    '# 使用二分法匹配灰度最相近的Ascii码 #
+    'Function AsciiFromGrayScale(ByVal GrayScale As Byte) As Char
+    '# 对图像进行去色处理 #
+    'Function GrayScaleBitmap(ByVal InitialBitmap As Bitmap) As Bitmap
+
+    Private Function GrayScaleBitmap(ByVal InitialBitmap As Bitmap) As Bitmap
+        Dim GrayBitmapData As Imaging.BitmapData = New Imaging.BitmapData
+        GrayBitmapData = InitialBitmap.LockBits(New Rectangle(0, 0, InitialBitmap.Width, InitialBitmap.Height),
+                Imaging.ImageLockMode.WriteOnly, InitialBitmap.PixelFormat)
+        Dim DataStride As Integer = GrayBitmapData.Stride
+        Dim DataHeight As Integer = GrayBitmapData.Height
+        Dim GrayDataArray(DataStride * DataHeight - 1) As Byte
+        Marshal.Copy(GrayBitmapData.Scan0, GrayDataArray, 0, GrayDataArray.Length)
+
+        Dim Index As Integer, Gray As Byte
+        For Index = 0 To GrayDataArray.Length - 1 Step 4
+            Gray = GrayDataArray(Index + 2) * 0.229 + GrayDataArray(Index + 1) * 0.587 + GrayDataArray(Index + 0) * 0.114
+            GrayDataArray(Index + 0) = Gray
+            GrayDataArray(Index + 1) = Gray
+            GrayDataArray(Index + 2) = Gray
+        Next
+
+        Marshal.Copy(GrayDataArray, 0, GrayBitmapData.Scan0, GrayDataArray.Length)
+        InitialBitmap.UnlockBits(GrayBitmapData)
+        Return InitialBitmap
+    End Function
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        If Not CustomImageDialog.ShowDialog = DialogResult.OK Then Exit Sub
+        TestBitmap = New Bitmap(CustomImageDialog.FileName)
+
+        Try
+            mFrameCount = TestBitmap.GetFrameCount(Imaging.FrameDimension.Time)
+        Catch ex As Exception
+            mFrameCount = 1
+        End Try
+        If mFrameCount = 1 Then
+            GIFTimer.Stop()
+            TestBitmap = GrayScaleBitmap(TestBitmap) '先转换为灰阶图像
+            TextBox1.Text = BitmapToAscii(TestBitmap, CellSize) '对图像分块进行Ascii灰度比对
+            TextBox1.SelectionStart = 0
+            TextBox1.SelectionLength = 0
+        Else
+            mFrameIndex = 0
+            mTimeDimension = New FrameDimension(TestBitmap.FrameDimensionsList.First)
+            GIFTimer.Start()
+        End If
+    End Sub
+    Private Sub ToolStripTextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles ToolStripTextBox1.KeyPress
+        If e.KeyChar <> Chr(Keys.Enter) Then Exit Sub
+        If IsNumeric(ToolStripTextBox1.Text) Then
+            CellSize.Width = Int(ToolStripTextBox1.Text.Trim)
+            CellSize.Height = CellSize.Width * 2
+            TextBox1.Text = BitmapToAscii(TestBitmap, CellSize) '对图像分块进行Ascii灰度比对
+        End If
+    End Sub
+
+    Private Sub 复制到剪切板ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 复制到剪切板ToolStripMenuItem.Click
+        My.Computer.Clipboard.SetText(TextBox1.Text)
+        Me.Text = "文本已经复制到剪切板！ - " & My.Computer.Clock.LocalTime
+    End Sub
+
+    Private Function BitmapToAscii(ByVal GrayScaleBitmap As Bitmap, ByVal CellSize As Size) As String
+        Dim AsciiString As String = vbNullString
+        Dim IndexX, IndexY As Integer
+        Dim CellBitmap As Bitmap
+        Dim CellGrayScale As Byte
+        For IndexY = 0 To GrayScaleBitmap.Height - 1 - CellSize.Height Step CellSize.Height
+            For IndexX = 0 To GrayScaleBitmap.Width - 1 - CellSize.Width Step CellSize.Width
+                CellBitmap = GrayScaleBitmap.Clone(New Rectangle(IndexX, IndexY, CellSize.Width, CellSize.Height), Imaging.PixelFormat.Format32bppArgb)
+                CellBitmap = New Bitmap(CellBitmap, 1, 1)
+                CellGrayScale = 255 - CellBitmap.GetPixel(0, 0).R
+                AsciiString &= AsciiFromGrayScale(CellGrayScale)
+            Next
+            AsciiString &= vbCrLf
+        Next
+
+        Return Strings.Left(AsciiString, AsciiString.Length - 1)
+    End Function
+
+    Private Function AsciiFromGrayScale(ByVal GrayScale As Byte) As Char
+        Dim lower As Byte = 0
+        Dim higher As Byte = AsciiCount
+        Dim Mid As Byte
+        While ((higher - lower) > 1)
+            Mid = (lower + higher) / 2
+            If (GrayScale > AscGray(Mid)) Then
+                lower = Mid
+            Else
+                higher = Mid
+            End If
+        End While
+        Return Ascii(lower)
+    End Function
+
+    Private Sub TextBox1_DoubleClick(sender As Object, e As EventArgs) Handles TextBox1.DoubleClick
+        MenuStrip1.Visible = Not MenuStrip1.Visible
+    End Sub
+
+    Private Sub GIFTimer_Tick(sender As Object, e As EventArgs) Handles GIFTimer.Tick
+        TestBitmap.SelectActiveFrame(mTimeDimension, mFrameIndex)
+
+        FrameBitmap = Nothing
+        FrameBitmap = New Bitmap(TestBitmap.Width, TestBitmap.Height)
+        FrameGraphics = Graphics.FromImage(FrameBitmap)
+        FrameGraphics.DrawImageUnscaled(TestBitmap, 0, 0)
+        FrameGraphics.Dispose()
+        FrameBitmap = GrayScaleBitmap(FrameBitmap) '先转换为灰阶图像
+        TextBox1.Text = BitmapToAscii(FrameBitmap, CellSize) '对图像分块进行Ascii灰度比对
+        TextBox1.SelectionStart = 0
+        TextBox1.SelectionLength = 0
+        Me.Text = mFrameIndex & " / " & mFrameCount
+        mFrameIndex = IIf(mFrameIndex = mFrameCount - 1, 0, mFrameIndex + 1)
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        Dim SaveBitmap As Bitmap = New Bitmap(1, 1)
+        Dim SaveGraphics As Graphics = Graphics.FromImage(SaveBitmap)
+        Dim FontHeight As Integer = TextBox1.Font.GetHeight(SaveGraphics)
+        Try
+            SaveBitmap = New Bitmap(TextBox1.Lines.First.Length * 6, FontHeight * (TextBox1.Lines.Count - 1))
+            SaveGraphics = Graphics.FromImage(SaveBitmap)
+            SaveGraphics.FillRectangle(Brushes.White, 0, 0, SaveBitmap.Width, SaveBitmap.Height)
+            SaveGraphics.DrawString(TextBox1.Text, TextBox1.Font, Brushes.Black, 0, 0)
+            SaveBitmap.Save(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "\" & My.Computer.Clock.TickCount & ".png", Imaging.ImageFormat.Png)
+            Me.Text = "文件已保存到桌面！ - " & My.Computer.Clock.LocalTime
+        Catch ex As Exception
+            Me.Text = ex.Message & " -  & My.Computer.Clock.LocalTime" & My.Computer.Clock.LocalTime
+        Finally
+            SaveGraphics.Dispose()
+            SaveBitmap = Nothing
+        End Try
+    End Sub
+
+End Class
